a31ff9a610c0eb106c563e3d42980475ffd7a5ad
ArindamDas28032004-sudo <arindamdas28032004@gmail.com>
2026-07-24 11:47:54 +0530

    Update CORS origin for production deployment

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://todo-app-backend-rirx.onrender.com',
   credentials:true
 }));
 app.use(cookieParser())
